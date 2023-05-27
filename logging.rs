0831340d68658871499a643b2708f00f7c6cc874
OccupyMars2025 <31559413+OccupyMars2025@users.noreply.github.com>
2023-05-27 09:42:20 +0800

    Update logging.rs

--- logging.rs
+++ logging.rs
@@ -2,6 +2,29 @@
 
 本模块利用 log crate 为你提供了日志功能，使用方式见 main.rs.
 Copy from: https://github.com/rcore-os/rCore-Tutorial-v3/blob/ch1/os/src/logging.rs
+Example: you can use this module to do colorful logging
+    logging::init();
+    
+    println!("[kernel] Hello, world!");
+    trace!(
+        "[kernel] .text [{:x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    debug!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    warn!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    error!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+
 */
 
 use log::{self, Level, LevelFilter, Log, Metadata, Record};
